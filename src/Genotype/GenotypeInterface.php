8b774fdefb672de7ea98d4fa7e51c4c58d4989ef
Soeren Parton <mail@floatingbitsmedia.de>
2021-08-30 16:38:34 +0200

    minor: comment why there is an empty interface

--- src/Genotype/GenotypeInterface.php
+++ src/Genotype/GenotypeInterface.php
@@ -7,6 +7,8 @@ namespace FloatingBits\EvolutionaryAlgorithm\Genotype;
 /**
  * Interface GenotypeInterface
  * @package FloatingBits\EvolutionaryAlgorithm\Genotype
+ * This interface exists as a marker to restrict the possible concrete interfaces passed to generic templates of the
+ * scheme template T0 of GenotypeInterface
  */
 interface GenotypeInterface
 {
