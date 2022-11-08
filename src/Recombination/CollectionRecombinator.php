04d8cb869cf7e3b75e4e3117bcef0787c5609d23
Soeren Parton <mail@floatingbitsmedia.de>
2022-11-08 13:31:39 +0100

    adding Collection recombinator

---
+++ src/Recombination/CollectionRecombinator.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace FloatingBits\EvolutionaryAlgorithm\Recombination;
+
+use FloatingBits\EvolutionaryAlgorithm\Randomizer\ConfigurableIntRandomizerInterface;
+use FloatingBits\EvolutionaryAlgorithm\Randomizer\IntRandomizerInterface;
+use FloatingBits\EvolutionaryAlgorithm\Specimen\SpecimenCollection;
+use FloatingBits\EvolutionaryAlgorithm\Specimen\SpecimenInterface;
+
+/**
+ * @template T0 of GenotypeInterface
+ * @template T1 of FitnessInterface
+ */
+class CollectionRecombinator implements CollectionRecombinatorInterface
+{
+    /**
+     * @var IndividualRecombinatorInterface<T0,T1>
+     */
+    private $individualRecombinator;
+    /**
+     * @var ConfigurableIntRandomizerInterface
+     */
+    private $intRandomizer;
+
+    /**
+     * @param $individualRecombinator
+     * @param ConfigurableIntRandomizerInterface $intRandomizer
+     */
+    public function __construct($individualRecombinator, ConfigurableIntRandomizerInterface $intRandomizer)
+    {
+        $this->individualRecombinator = $individualRecombinator;
+        $this->intRandomizer = $intRandomizer;
+    }
+
+    /**
+     * @param SpecimenCollection $specimenCollection
+     * @param int $populationSize
+     * @return SpecimenCollection
+     */
+    public function recombine(SpecimenCollection $specimenCollection, int $populationSize): SpecimenCollection
+    {
+        $currentCount = $originalCount = count($specimenCollection);
+        while ($currentCount < $populationSize) {
+            /** @var SpecimenInterface $specimen1, $specimen2 */
+            list($specimen1, $specimen2) = $this->chooseRecombinationCouple($specimenCollection, $originalCount);
+            $newGenotype = $this->individualRecombinator->recombine(
+                $specimen1->getGenotype(), $specimen2->getGenotype(),
+                $specimen1->getEvaluation(), $specimen2->getEvaluation()
+            );
+            $newSpecimen = clone $specimen1;
+            $newSpecimen->setGenotype($newGenotype);
+            $specimenCollection->addSpecimen($newSpecimen);
+            $currentCount++;
+        }
+        return $specimenCollection;
+    }
+
+    /**
+     * @param SpecimenCollection $specimenCollection
+     * @param int $chooseFromNFirstIndeces
+     * @return array
+     */
+    private function chooseRecombinationCouple(SpecimenCollection $specimenCollection, int $chooseFromNFirstIndeces) {
+        $this->intRandomizer->setMax($chooseFromNFirstIndeces - 1);
+        $index1 = $this->intRandomizer->randomInt();
+        $index2 = $this->intRandomizer->randomInt();
+        while ($index1 === $index2 && $chooseFromNFirstIndeces > 1) {
+            //Only go here if collection has at least two specimen.
+            $index2 = $this->intRandomizer->randomInt();
+        }
+        return [$specimenCollection->getSpecimen($index1), $specimenCollection->getSpecimen($index2)];
+    }
+
+
+
+}
---
+++ tests/unit/Recombination/CollectionRecombinatorTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace FloatingBits\EvolutionaryAlgorithm\Tests\unit\Recombination;
+
+use FloatingBits\EvolutionaryAlgorithm\Randomizer\IntRandomizer;
+use FloatingBits\EvolutionaryAlgorithm\Recombination\CollectionRecombinator;
+use FloatingBits\EvolutionaryAlgorithm\Recombination\SymbolArrayCrossoverRecombinator;
+use FloatingBits\EvolutionaryAlgorithm\Specimen\Specimen;
+use FloatingBits\EvolutionaryAlgorithm\Specimen\SpecimenCollection;
+use PHPUnit\Framework\TestCase;
+
+class CollectionRecombinatorTest extends TestCase
+{
+    public function testRecombine() {
+        $intRandomizer = $this->createMock(IntRandomizer::class);
+        $intRandomizer->method('randomInt')->willReturnOnConsecutiveCalls(1,2);
+        $individualRecombinator = $this->createMock(SymbolArrayCrossoverRecombinator::class);
+        $recombinator = new CollectionRecombinator($individualRecombinator, $intRandomizer);
+        $specimenCollection = $this->createMock(SpecimenCollection::class);
+        $specimenCollection->method('count')->willReturn(2);
+        $specimen1 = $this->createMock(Specimen::class);
+        $specimen2 = $this->createMock(Specimen::class);
+        $specimenCollection->method('getSpecimen')->willReturnCallback(function(int $index) use ($specimen1, $specimen2) {
+            if ($index === 0) {
+                return $specimen1;
+            }
+            return $specimen2;
+        });
+        $specimenCollection->expects($this->once())->method('addSpecimen');
+        $individualRecombinator->expects($this->once())->method('recombine');
+
+        $newCollection = $recombinator->recombine($specimenCollection, 3);
+
+
+    }
+}
