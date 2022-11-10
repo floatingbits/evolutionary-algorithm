7bd6e689c8c850b40117981d68ce791348c8b260
Soeren Parton <mail@floatingbitsmedia.de>
2022-11-10 10:41:39 +0100

    adding tournament class

---
+++ src/Evolution/Tournament.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace FloatingBits\EvolutionaryAlgorithm\Evolution;
+
+use FloatingBits\EvolutionaryAlgorithm\Specimen\SpecimenCollection;
+use FloatingBits\EvolutionaryAlgorithm\Specimen\SpecimenGeneratorInterface;
+
+class Tournament
+{
+    /** @var EvolverInterface */
+    private $evolver;
+
+    /** @var SpecimenCollection */
+    private $specimenCollection;
+
+    /** @var SpecimenGeneratorInterface */
+    private $specimenGenerator;
+
+    /**
+     * @param EvolverInterface $evolver
+     */
+    public function __construct(EvolverInterface $evolver, SpecimenGeneratorInterface $specimenGenerator) {
+        $this->evolver = $evolver;
+        $this->specimenGenerator = $specimenGenerator;
+    }
+
+    public function setup(int $populationSize) {
+        $this->specimenCollection = $this->specimenGenerator->generateSpecimen($populationSize);
+    }
+
+    public function runTournament($numRounds) {
+        for ($i= 0; $i<$numRounds; $i++) {
+            $this->specimenCollection = $this->evolver->evolve($this->specimenCollection);
+        }
+    }
+
+    /**
+     * @return SpecimenCollection
+     */
+    public function getSpecimenCollection() {
+        return $this->specimenCollection;
+    }
+
+
+}
